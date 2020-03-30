94dfd8998a07a90703608392b17ac58e65c32278
seizze <cwcwc3@gmail.com>
2020-03-30 23:57:44 +0900

    [#7] feat: Notification이 오면 측정소 업데이트

--- iOS/Dust/Dust/Controllers/DustViewController.swift
+++ iOS/Dust/Dust/Controllers/DustViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class DustViewController: UIViewController {
     
+    @IBOutlet weak var stationLabel: UILabel!
+    
     private let locationManagerDelegate = LocationManagerDelegate(with: DustViewModel())
+    private let observers = Observers()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addViewUpdatingObservers()
+    }
+    
+    deinit {
+        observers.removeObservers()
+    }
+    
+    private func addViewUpdatingObservers() {
+        observers.addObserver(forName: .stationDidUpdate) { [weak self] in
+            guard let event = $0 as? UpdateEvent else { return }
+            if case let .station(name: name) = event {
+                self?.stationLabel.text = name
+            }
+        }
     }
 }
