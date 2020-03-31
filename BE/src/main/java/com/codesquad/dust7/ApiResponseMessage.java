6fce6c3921844f62c0caaed071061a01033f9782
haveagood@naver.com <haveagood@naver.com>
2020-03-31 14:42:29 +0900

    [#9] feat : 응답 메세지를 전달하기 위한 ApiResponseMessage 클래스 추가
    - 요청이 들어왔을 때, 해당 요청에 대한 응답 메세지, 응답 상태등을 전달하기 위한 클래스를 생성하였습니다.
    
    # 본문 작성
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- BE/src/main/java/com/codesquad/dust7/ApiResponseMessage.java
+++ BE/src/main/java/com/codesquad/dust7/ApiResponseMessage.java
@@ -1,4 +1,26 @@
 package com.codesquad.dust7;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.json.JSONArray;
+import org.springframework.http.HttpStatus;
+
 public class ApiResponseMessage {
+    private HttpStatus status;
+    private Object message;
+    private int statusCode;
+
+    private ApiResponseMessage() {}
+
+    public ApiResponseMessage(HttpStatus status, String message, int statusCode) {
+        this();
+        this.status = status;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+    public ApiResponseMessage(HttpStatus status, JSONArray jsonArray, int statusCode) {
+        this();
+        this.status = status;
+        this.message = jsonArray;
+        this.statusCode = statusCode;
+    }
 }
