4e11fc0495d8c1bdd1d5d3b40e114c0dcd206920
haveagood@naver.com <haveagood@naver.com>
2020-03-31 18:41:35 +0900

    [#11] feat : 미세먼지 예보값에 필요한 변수 설정 및 클래스 변수 생성
    - raw데이터를 보고, 서비스를 구성하기 위해 필요한 변수들을 특정하였다
    
    - 변수들을 클래스에 선언하였습니다
    
    #11
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

--- BE/src/main/java/com/codesquad/dust7/DailyAirCondition.java
+++ BE/src/main/java/com/codesquad/dust7/DailyAirCondition.java
@@ -1,4 +1,28 @@
 package com.codesquad.dust7;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class DailyAirCondition {
+    private String dataTime;
+    private String imageUrl1;
+    private String imageUrl2;
+    private String imageUrl3;
+    private String informGrade;
+    private String informCause;
+    private String informOverall;
+
+    public DailyAirCondition(String dataTime, String imageUrl1, String imageUrl2, String imageUrl3, String informGrade, String informCause, String informOverall) {
+        this.dataTime = dataTime;
+        this.imageUrl1 = imageUrl1;
+        this.imageUrl2 = imageUrl2;
+        this.imageUrl3 = imageUrl3;
+        this.informGrade = informGrade;
+        this.informCause = informCause;
+        this.informOverall = informOverall;
+    }
 }
