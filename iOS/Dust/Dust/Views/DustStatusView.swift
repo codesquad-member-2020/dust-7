9918bf4cbfb9fd2843972e9ef3319e9c7aeca2cd
seizze <cwcwc3@gmail.com>
2020-04-02 18:51:42 +0900

    [#17] feat: 미세먼지 상태를 보여줄 커스텀뷰 추가

---
+++ iOS/Dust/Dust/Views/DustStatusView.swift
@@ -0,0 +1,18 @@
+//
+//  DustGotoHell.swift
+//  Dust
+//
+//  Created by Chaewan Park on 2020/04/02.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class DustStatusView: UIView {
+    @IBOutlet var statusImageView: UIImageView!
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var concentrationLabel: UILabel!
+    @IBOutlet var observationTimeLabel: UILabel!
+    @IBOutlet var stationLabel: UILabel!
+    
+}
