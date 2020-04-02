4245264cc1c9436dcc6d574555ca127efe3eb63d
seizze <cwcwc3@gmail.com>
2020-04-02 16:03:45 +0900

    [#17] feat: 사용할 컬러들 선언

---
+++ iOS/Dust/Dust/Utils/UIColorExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColorExtension.swift
+//  Dust
+//
+//  Created by Chaewan Park on 2020/04/02.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let detailTextGray = UIColor(named: "detailTextGray")
+    static let goodBlue = UIColor(named: "goodBlue")
+    static let moderateGreen = UIColor(named: "moderateGreen")
+    static let unhealthyOrange = UIColor(named: "unhealthyOrange")
+    static let hazardousRed = UIColor(named: "hazardousRed")
+}
