91945b3989ea03b2fd1233a2d1a5317181b2922a
Lime-Lee <prestoferoce@gmail.com>
2020-04-03 17:03:04 +0900

    [#29] 서버에서 받은 미세먼지 데이터 가공 후 렌더링

--- FE/js/dust/dust.js
+++ FE/js/dust/dust.js
@@ -27,8 +27,91 @@ async function getDustData(stationName) {
 
 function render(stationName, dustData) {
     renderStation(stationName);
+    renderGraph(dustData);
 }
 
 function renderStation(stationName) {
     TARGET.location.innerText = stationName;
+}
+
+function renderGraph(dustData) {
+    dustData.forEach(dustData => {
+        if (dustData.pm10Grade === "") return;
+        console.log(dustData);
+        const widthValue = calculateValue(parseInt(dustData.pm10Value));
+        const li = document.createElement("li");
+        const div = document.createElement("div");
+        const color = ['#017AFF', '#37E030', '#E0AA31', '#CE1817'];
+        const dustColor = parseInt(dustData.pm10Grade) - 1;
+        div.style.background = "linear-gradient(to right, " + color[dustColor] + " " + widthValue + "%, #FFFFFF " + widthValue + "%)";
+        const text = document.createTextNode(dustData.pm10Value);
+        div.appendChild(text);
+        li.appendChild(div);
+        document.querySelector('.dust-graph-ul').appendChild(li);
+    });
+    rendurDust(dustData);
+}
+
+function calculateValue(pm10Value) {
+    if (pm10Value < 201) return pm10Value / 2;
+    else if (pm10Value > 200) return 100;
+}
+
+function rendurDust(data) {
+    const emoji = document.querySelector(".dust-level-emoji");
+    const gradeText = document.querySelector(".dust-level-text");
+    const dustValue = document.querySelector(".dust-level-value");
+    const time = document.querySelector(".dust-level-time");
+    const dustTop = document.querySelector(".dust-top");
+
+    // 오늘 날짜
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth();
+    let date = today.getDate();
+
+    month = (month + 1);
+
+    if(month < 10) {
+        month = '0' + month;
+    }
+
+    if(date<10) {
+        date = '0' + date;
+    }
+
+    today = year + '-' + month + '-' + date;
+
+        if(data[0].dataTime.indexOf(today)) {
+            let nowTime = data[0].dataTime.substring(10, 16);
+            time.innerHTML = '오늘' + nowTime;
+        }else {
+            time.innerHTML = data[0].dataTime;
+        }
+    
+        switch (data[0].pm10Grade) {
+            case '1' :
+                emoji.innerHTML = '😀';
+                dustTop.style.background = 'linear-gradient(to top, white, #6096D8)';
+                break;
+            case '2' :
+                emoji.innerHTML = '🙂';
+                dustTop.style.background = 'linear-gradient(to top, white, #088A68)';
+                break;
+            case '3' :
+                emoji.innerHTML = '😷';
+                dustTop.style.background = 'linear-gradient(to top, white, #FAAC58)';
+                break;
+            case '4' :
+                emoji.innerHTML = '😱';
+                dustTop.style.background = 'linear-gradient(to top, white, #FA5858)';
+                break;
+            default :
+                emoji.innerHTML = '';
+                break;
+        }
+    
+        gradeText.innerHTML = data[0].pmMessage;
+        dustValue.innerHTML = data[0].pm10Value;
+    
 }
--- FE/js/dust/dustUtil.js
+++ FE/js/dust/dustUtil.js
@@ -1,5 +1,6 @@
 import { $ } from "../common/util.js";
 
 export const TARGET = {
-    location: $('.dust-location')
+    location: $('.dust-location'),
+    graphUl: $('.dust-graph-ul')
 }
